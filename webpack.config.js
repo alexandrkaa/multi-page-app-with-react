43f9133399cffa00c64e3e96a08c573059a6df00
Alexander Komarov <komar81@gmail.com>
2020-05-03 10:54:37 +0300

    update packeges version + fix linter errors in web[ackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,61 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const getFilesFromDir = require("./config/files");
-const PAGE_DIR = path.join("src", "pages", path.sep);
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const getFilesFromDir = require('./config/files');
 
-const htmlPlugins = getFilesFromDir(PAGE_DIR, [".html"]).map( filePath => {
-  const fileName = filePath.replace(PAGE_DIR, "");
-  // { chunks:["contact", "vendor"], template: "src/pages/contact.html",  filename: "contact.html"}
+const PAGE_DIR = path.join('src', 'pages', path.sep);
+
+const htmlPlugins = getFilesFromDir(PAGE_DIR, ['.html']).map((filePath) => {
+  const fileName = filePath.replace(PAGE_DIR, '');
+  // { chunks:['contact', 'vendor'], template: 'src/pages/contact.html',  filename: 'contact.html'}
   return new HtmlWebPackPlugin({
-    chunks:[fileName.replace(path.extname(fileName), ""), "vendor"],
+    chunks: [fileName.replace(path.extname(fileName), ''), 'vendor'],
     template: filePath,
-    filename: fileName
-  })
+    filename: fileName,
+  });
 });
 
-// { contact: "./src/pages/contact.js" }
-const entry = getFilesFromDir(PAGE_DIR, [".js"]).reduce( (obj, filePath) => {
-  const entryChunkName = filePath.replace(path.extname(filePath), "").replace(PAGE_DIR, "");
+// { contact: './src/pages/contact.js' }
+const entry = getFilesFromDir(PAGE_DIR, ['.js']).reduce((obj, filePath) => {
+  const entryChunkName = filePath.replace(path.extname(filePath), '').replace(PAGE_DIR, '');
   obj[entryChunkName] = `./${filePath}`;
   return obj;
-}, {}); 
+}, {});
 
 module.exports = (env, argv) => ({
   entry: entry,
   output: {
-    path: path.join(__dirname, "build"),
-    filename: "[name].[hash:4].js"
+    path: path.join(__dirname, 'build'),
+    filename: '[name].[hash:4].js'
   },
   devtool: argv.mode === 'production' ? false : 'eval-source-maps',
   plugins: [
-    ...htmlPlugins
+    ...htmlPlugins,
   ],
-  resolve:{
-		alias:{
-			src: path.resolve(__dirname, "src"),
-			components: path.resolve(__dirname, "src", "components")
-		}
-	},
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+      components: path.resolve(__dirname, 'src', 'components')
+    }
+  },
   module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader:"babel-loader",
-					options:{
-						presets: [
-							"@babel/preset-env",
-							"@babel/preset-react"
-						], 
-					}
-				},
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ],
+          }
+        },
       },
       {
-				test: /\.css$/,
-				use: ["style-loader", {loader: "css-loader", options: {modules: true}}],
-				exclude: /node_modules/,
+        test: /\.css$/,
+        use: ['style-loader', { loader: 'css-loader', options: { modules: true } }],
+        exclude: /node_modules/,
       },
       {
         test: /\.(svg|jpg|gif|png)$/,
@@ -64,7 +65,7 @@ module.exports = (env, argv) => ({
             options: {
               name: '[name].[ext]',
               outputPath: (url, resourcePath, context) => {
-                if(argv.mode === 'development') {
+                if (argv.mode === 'development') {
                   const relativePath = path.relative(context, resourcePath);
                   return `/${relativePath}`;
                 }
@@ -78,10 +79,10 @@ module.exports = (env, argv) => ({
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           {
-            loader: 'file-loader', 
+            loader: 'file-loader',
             options: {
               outputPath: (url, resourcePath, context) => {
-                if(argv.mode === 'development') {
+                if (argv.mode === 'development') {
                   const relativePath = path.relative(context, resourcePath);
                   return `/${relativePath}`;
                 }
@@ -91,18 +92,18 @@ module.exports = (env, argv) => ({
           }
         ]
       }]
+  },
+  optimization: {
+    minimize: argv.mode === 'production' ? true : false,
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          chunks: 'initial',
+          name: 'vendor',
+          enforce: true
+        },
+      },
     },
-    optimization: {
-      minimize: argv.mode === 'production' ? true : false,
-      splitChunks: {
-        cacheGroups: {
-          vendor: {
-            test: /node_modules/,
-            chunks: "initial",
-            name: "vendor",
-            enforce: true
-          }
-        }
-      }
-    }
+  },
 });
